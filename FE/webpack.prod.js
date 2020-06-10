7210dbf1f7963558fd411bd60e6092a953a26239
최성익 <tjddlr92@gmail.com>
2020-06-10 17:12:22 +0900

    [FE/#2] Fix : webpack prod 에러 수정
    - splitChunks 를 TereserPlugin 바깥으로 이동
    - CleanWebpackPlugin 사용을 npm 문서에 작성돼있는 방식대로 변경
    - CleanWebpackPlugin 생성자 내부에 dist 삭제. 있으면 작동하지 않음

--- FE/webpack.prod.js
+++ FE/webpack.prod.js
@@ -1,19 +1,23 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const TerserPlugin = require("terser-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
 
   devtool: "cheap-module-source-map",
 
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "[name].bundle.js",
-  },
-
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
     minimize: true,
     minimizer: [
       new TerserPlugin({
@@ -23,18 +27,9 @@ module.exports = merge(common, {
             drop_console: true,
           },
         },
-        splitChunks: {
-          cacheGroups: {
-            commons: {
-              test: /[\\/]node_modules[\\/]/,
-              name: "vendors",
-              chunks: "all",
-            },
-          },
-        },
       }),
     ],
   },
 
-  plugins: [new CleanWebpackPlugin(["dist"])],
+  plugins: [new CleanWebpackPlugin()],
 });
