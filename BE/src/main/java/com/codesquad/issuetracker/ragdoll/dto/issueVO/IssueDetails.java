e6b2367db7f812421da41ce6d7af849086c4d872
ragdoll <museopkim0214@gmail.com>
2020-06-12 06:56:12 +0900

    [BE/#17] Feat : DTO와 연동 되는 이슈 디테일 정보 관련 클래스 추가
    - issueId : 이슈의 아이디 값
    - issueTitle : 이슈 제목
    - milestone : 소속 마일스톤 정보
    - attachedLabels : 해당 이슈에 할당 된 레이블 정보
    - author : 해당 이슈의 작성자
    - allocatedAssignees : 해당 이슈에 할당 된 담당자 정보
    - createdAt : 이슈 생성 날짜 및 시간 정보
    - opened : open되어 있는 이슈인지, closed 되어 있는 이슈인지에 관한 정보와 연관

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dto/issueVO/IssueDetails.java
@@ -0,0 +1,159 @@
+package com.codesquad.issuetracker.ragdoll.dto.issueVO;
+
+import com.codesquad.issuetracker.ragdoll.dto.UserVO.UserSummary;
+import com.codesquad.issuetracker.ragdoll.dto.labelVO.LabelDetails;
+import com.codesquad.issuetracker.ragdoll.dto.milestoneVO.MilestoneSummary;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class IssueDetails {
+
+    private Long issueId;
+
+    private String issueTitle;
+
+    private MilestoneSummary milestone;
+
+    private List<LabelDetails> attachedLabels;
+
+    private UserSummary author;
+
+    private List<UserSummary> allocatedAssignees;
+
+    private LocalDateTime createdAt;
+
+    private boolean opened;
+
+    public IssueDetails() {}
+
+    private IssueDetails(Long issueId, String issueTitle, MilestoneSummary milestone, List<LabelDetails> attachedLabels, UserSummary author, List<UserSummary> allocatedAssignees, LocalDateTime createdAt, boolean opened) {
+        this.issueId = issueId;
+        this.issueTitle = issueTitle;
+        this.milestone = milestone;
+        this.attachedLabels = attachedLabels;
+        this.author = author;
+        this.allocatedAssignees = allocatedAssignees;
+        this.createdAt = createdAt;
+        this.opened = opened;
+    }
+
+    public Long getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(Long issueId) {
+        this.issueId = issueId;
+    }
+
+    public String getIssueTitle() {
+        return issueTitle;
+    }
+
+    public void setIssueTitle(String issueTitle) {
+        this.issueTitle = issueTitle;
+    }
+
+    public MilestoneSummary getMilestone() {
+        return milestone;
+    }
+
+    public void setMilestone(MilestoneSummary milestone) {
+        this.milestone = milestone;
+    }
+
+    public List<LabelDetails> getAttachedLabels() {
+        return attachedLabels;
+    }
+
+    public void setAttachedLabels(List<LabelDetails> attachedLabels) {
+        this.attachedLabels = attachedLabels;
+    }
+
+    public UserSummary getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserSummary author) {
+        this.author = author;
+    }
+
+    public List<UserSummary> getAllocatedAssignees() {
+        return allocatedAssignees;
+    }
+
+    public void setAllocatedAssignees(List<UserSummary> allocatedAssignees) {
+        this.allocatedAssignees = allocatedAssignees;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public void setOpened(boolean opened) {
+        this.opened = opened;
+    }
+
+    public static class Builder {
+        private Long issueId;
+        private String issueTitle;
+        private MilestoneSummary milestone;
+        private List<LabelDetails> attachedLabels;
+        private UserSummary author;
+        private List<UserSummary> allocatedAssignees;
+        private LocalDateTime createdAt;
+        private boolean opened;
+
+        public Builder issueId(Long issueId) {
+            this.issueId = issueId;
+            return this;
+        }
+
+        public Builder issueTitle(String issueTitle) {
+            this.issueTitle = issueTitle;
+            return this;
+        }
+
+        public Builder milestone(MilestoneSummary milestone) {
+            this.milestone = milestone;
+            return this;
+        }
+
+        public Builder attachedLabels(List<LabelDetails> attachedLabels) {
+            this.attachedLabels = attachedLabels;
+            return this;
+        }
+
+        public Builder author(UserSummary author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder allocatedAssignees(List<UserSummary> allocatedAssignees) {
+            this.allocatedAssignees = allocatedAssignees;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder opened(boolean opened) {
+            this.opened = opened;
+            return this;
+        }
+
+        public IssueDetails build() {
+            return new IssueDetails(issueId, issueTitle, milestone, attachedLabels, author, allocatedAssignees, createdAt, opened);
+        }
+    }
+}
