fb8c281e330515aaa0013371761e1835e1ac880f
ragdoll <museopkim0214@gmail.com>
2020-06-12 16:24:16 +0900

    [BE/#17] Feat : UserDao 로직 구현
    - Asigneees
    - Users

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dao/UserDao.java
@@ -0,0 +1,55 @@
+package com.codesquad.issuetracker.ragdoll.dao;
+
+import com.codesquad.issuetracker.ragdoll.domain.User;
+import com.codesquad.issuetracker.ragdoll.dto.userVO.UserSummary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<UserSummary> findAllocatedAssigneesByIds(Long issueId) {
+        String sql = "SELECT u.id, u.name, u.avatar_url " +
+                     "FROM user u JOIN assignee a ON u.id = a.user_id " +
+                     "WHERE a.issue_id = ?";
+        return jdbcTemplate.query(sql, new Object[] {issueId},
+                (rs, rowNum) -> UserSummary.create(rs.getLong("u.id"), rs.getString("u.name"),
+                                                   rs.getString("u.avatar_url")));
+    }
+
+    public User findUserById(Long userId) {
+        String sql = "SELECT id, name, email, github_id, avatar_url, created_date_time " +
+                     "FROM user WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{userId},
+                (rs, rowNum) -> new User.Builder()
+                                        .id(rs.getLong("id"))
+                                        .name(rs.getString("name"))
+                                        .email(rs.getString("email"))
+                                        .githubId(rs.getLong("github_id"))
+                                        .avatarUrl(rs.getString("avatar_url"))
+                                        .createdDateTime(rs.getTimestamp("created_date_time").toLocalDateTime())
+                                        .build());
+    }
+
+    public List<User> findAllUsers() {
+        String sql = "SELECT id, name, email, github_id, avatar_url, created_date_time " +
+                     "FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> new User.Builder()
+                                                               .id(rs.getLong("id"))
+                                                               .name(rs.getString("name"))
+                                                               .email(rs.getString("email"))
+                                                               .githubId(rs.getLong("github_id"))
+                                                               .avatarUrl(rs.getString("avatar_url"))
+                                                               .createdDateTime(rs.getTimestamp("created_date_time").toLocalDateTime())
+                                                               .build());
+    }
+}
