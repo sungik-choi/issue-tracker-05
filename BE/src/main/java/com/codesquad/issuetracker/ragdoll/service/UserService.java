d93bc4ce5b239930c56f7b568df85f72e128ecfb
ragdoll <museopkim0214@gmail.com>
2020-06-12 12:25:21 +0900

    [BE/#17] Feat : UserService 로직 설계
    - SQL을 최대한 작은 단위로 활용
    - 재사용성 고려

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/service/UserService.java
@@ -0,0 +1,34 @@
+package com.codesquad.issuetracker.ragdoll.service;
+
+import com.codesquad.issuetracker.ragdoll.dao.UserDao;
+import com.codesquad.issuetracker.ragdoll.domain.User;
+import com.codesquad.issuetracker.ragdoll.dto.userVO.UserSummary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<UserSummary> findAllocatedAssigneesByIds(Long issueId) {
+        return userDao.findAllocatedAssigneesByIds(issueId);
+    }
+
+    public User findUserById(Long userId) {
+        return userDao.findUserById(userId);
+    }
+
+    public List<UserSummary> findAllAssignees() {
+        List<User> allUsers = userDao.findAllUsers();
+        return allUsers.stream()
+                       .map(user -> UserSummary.create(user.getId(), user.getName(), user.getAvatarUrl()))
+                       .collect(Collectors.toList());
+    }
+}
