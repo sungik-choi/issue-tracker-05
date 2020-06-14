a1c3649378859f45e868cd48491c009111f97f18
ragdoll <museopkim0214@gmail.com>
2020-06-15 03:44:30 +0900

    [BE/#18] Feat : 표준 응답 객체에 CREATED 응답 메서드 추가
    - 데이터 생성시 CREATED 메서드로 응답 결과를 반환 하도록 수정

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/response/ApiResponse.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/response/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.codesquad.issuetracker.ragdoll.response;
 
+import com.codesquad.issuetracker.ragdoll.commonconstant.ResponseMessages;
+
 public class ApiResponse<T> {
 
     private boolean status;
@@ -15,5 +17,23 @@ public class ApiResponse<T> {
 
     public static <T> ApiResponse<T> OK(T response) {
         return new ApiResponse(true, response);
+    }
+
+    public static <T> ApiResponse<T> CREATED(T response) {return new ApiResponse(true, response); }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
     }
 }
