747588e0356deee282c64d7e0e7ddd2ea8399f4c
ragdoll <museopkim0214@gmail.com>
2020-06-15 03:46:33 +0900

    [BE/#18] Feat : 이슈 등록 기능 추가
    - 성공 시 ApiResponse.CREATED() 로 RESPONSE BODY 반환
    - 상태코드 201로 반환 하도록 설정

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/controller/IssueController_Ragdoll.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/controller/IssueController_Ragdoll.java
@@ -1,13 +1,12 @@
 package com.codesquad.issuetracker.ragdoll.controller;
 
 import com.codesquad.issuetracker.ragdoll.dto.ListOfIssuesDto;
+import com.codesquad.issuetracker.ragdoll.dto.SubmitNewIssueRequestDto;
 import com.codesquad.issuetracker.ragdoll.response.ApiResponse;
 import com.codesquad.issuetracker.ragdoll.service.IssueService_Ragdoll;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ragdoll")
@@ -21,6 +20,11 @@ public class IssueController_Ragdoll {
 
     @GetMapping("/issues")
     public ResponseEntity<ApiResponse<ListOfIssuesDto>> listing() {
-        return new ResponseEntity(issueService.findAllIssues(), HttpStatus.OK);
+        return new ResponseEntity(ApiResponse.OK(issueService.findAllIssues()), HttpStatus.OK);
+    }
+
+    @PostMapping("/issues")
+    public ResponseEntity<ApiResponse<String>> submit(@RequestBody SubmitNewIssueRequestDto submitNewIssueRequestDto) {
+        return new ResponseEntity(ApiResponse.CREATED(issueService.submitNewIssue(submitNewIssueRequestDto)), HttpStatus.CREATED);
     }
 }
