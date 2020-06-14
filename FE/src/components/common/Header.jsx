9c272ed432376823542cde6b42febb37a12ed7f1
Elllin <noto01@naver.com>
2020-06-15 00:04:02 +0900

    [FE/#3] Feat : material ui icons 설치

--- FE/src/components/common/Header.jsx
+++ FE/src/components/common/Header.jsx
@@ -3,13 +3,27 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-  return <HeaderWrap>a</HeaderWrap>;
+  return (
+    <HeaderWrap>
+      <Title>lssues</Title>
+    </HeaderWrap>
+  );
 };
 
 const HeaderWrap = styled.header`
+  display: flex;
+  justify-content: center;
   width: 100%;
-  height: 30%;
+  height: 55px;
   background-color: #24292e;
+  padding: 0 16px;
+  line-height: 55px;
+`;
+
+const Title = styled.span`
+  color: hsla(0, 0%, 100%, 0.7);
+  font-weight: 600;
+  font-size: 23px;
 `;
 
 export default Header;
