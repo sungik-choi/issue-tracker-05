8106087c0a76f992778120d9e4412544d60715d7
ragdoll <museopkim0214@gmail.com>
2020-06-15 03:35:09 +0900

    [BE/#18] Feat : 새로운 이슈 생성 및 그에 따른 코멘트 생성 쿼리 추가
    - submitNewIssue() : 새로운 이슈 생성 (생성 된 이슈의 id 값 반환)
    - registerNewComment() : 이슈 생성에 따른 코멘트 생성

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/dao/IssueDao_Ragdoll.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dao/IssueDao_Ragdoll.java
@@ -2,10 +2,16 @@ package com.codesquad.issuetracker.ragdoll.dao;
 
 import com.codesquad.issuetracker.ragdoll.domain.Issue;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,8 +19,11 @@ public class IssueDao_Ragdoll {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     public IssueDao_Ragdoll(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
 
@@ -29,5 +38,30 @@ public class IssueDao_Ragdoll {
                                                                 .userId(rs.getLong("user_id"))
                                                                 .milestoneId(rs.getInt("milestone_id"))
                                                                 .build());
+    }
+
+    public Long submitNewIssue(Long authorId, String title) {
+        String sql = "INSERT INTO issue (title, created_date_time, is_opened, user_id, milestone_id) " +
+                     "VALUES (:title, :createdDateTime, :isOpened, :authorId, :milestoneId)";
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                                                .addValue("title", title)
+                                                .addValue("createdDateTime", LocalDateTime.now())
+                                                .addValue("isOpened", true)
+                                                .addValue("authorId", authorId)
+                                                .addValue("milestoneId", null);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        Number registeredNewIssueId = namedParameterJdbcTemplate.update(sql, namedParameters, keyHolder, new String[]{"registeredNewIssueId"});
+        return registeredNewIssueId.longValue();
+    }
+
+    public void registerNewComment(Long commenterId, Long issueId, String description) {
+        String sql = "INSERT INTO comment (description, created_date_time, issue_id, user_id) " +
+                     "VALUES (:description, :createdDateTime, :issueId, :commenterId)";
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                                                .addValue("description", description)
+                                                .addValue("createdDateTime", LocalDateTime.now())
+                                                .addValue("issueId", issueId)
+                                                .addValue("commenterId", commenterId);
+        namedParameterJdbcTemplate.update(sql, namedParameters);
     }
 }
