047ffe82fe99b726b928ad34298fe517a4e3650a
최성익 <tjddlr92@gmail.com>
2020-06-15 18:30:09 +0900

    [FE/#8] Fix : id 타입 변경
    - string -> number

--- FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
+++ FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 
-import Label from "@Components/Label";
+import Label from "@Components/common/Label";
 import Title from "./Title";
 import Details from "./Details";
 
@@ -14,7 +14,7 @@ const Contents = () => {
           <Title title="[FE] 개발환경 구축하기" />
           <Label name="레이블 내용" backgroundColor="#000" color="#FFF" />
         </Box>
-        <Details id="18" time="4day ago" author="sungik-choi" />
+        <Details id={18} time="4day ago" author="sungik-choi" />
       </Box>
     </>
   );
