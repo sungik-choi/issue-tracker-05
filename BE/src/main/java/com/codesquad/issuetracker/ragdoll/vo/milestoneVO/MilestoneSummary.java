1de99420fd8378abd12cc1a314e584808c3a5eb9
ragdoll <museopkim0214@gmail.com>
2020-06-16 01:17:54 +0900

    [BE/#28] Feat : MilestoneSummary 클래스에 progress 필드 값 추가
    - progress : 마일스톤 진행률 반환을 위한 필드 값 추가

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/vo/milestoneVO/MilestoneSummary.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/vo/milestoneVO/MilestoneSummary.java
@@ -6,6 +6,8 @@ public class MilestoneSummary {
 
     private String milestoneTitle;
 
+    private Double progress;
+
     public MilestoneSummary() {}
 
     public MilestoneSummary(Integer milestoneId, String milestoneTitle) {
@@ -13,8 +15,18 @@ public class MilestoneSummary {
         this.milestoneTitle = milestoneTitle;
     }
 
+    public MilestoneSummary(Integer milestoneId, String milestoneTitle, Double progress) {
+        this.milestoneId = milestoneId;
+        this.milestoneTitle = milestoneTitle;
+        this.progress = progress;
+    }
+
     public static MilestoneSummary create(Integer milestoneId, String milestoneTitle) {
         return new MilestoneSummary(milestoneId, milestoneTitle);
+    }
+
+    public static MilestoneSummary create(Integer milestoneId, String milestoneTitle, Double progress) {
+        return new MilestoneSummary(milestoneId, milestoneTitle, progress);
     }
 
     public Integer getMilestoneId() {
@@ -31,5 +43,13 @@ public class MilestoneSummary {
 
     public void setMilestoneTitle(String milestoneTitle) {
         this.milestoneTitle = milestoneTitle;
+    }
+
+    public Double getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Double progress) {
+        this.progress = progress;
     }
 }
