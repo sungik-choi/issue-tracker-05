5f95b64ac8541525c0ad7ff74bcabbac57d24a26
최성익 <tjddlr92@gmail.com>
2020-06-15 18:18:54 +0900

    [FE/#8] Feat : Title 컴포넌트 props 받게 변경

--- FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
+++ FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 
-import Label from "@Components/Labels";
+import Label from "@Components/Label";
 import Title from "./Title";
 import Details from "./Details";
 
@@ -11,7 +11,7 @@ const Contents = () => {
     <>
       <Box display="flex" flexDirection="column" marginLeft={2}>
         <Box display="flex" alignItems="center">
-          <Title />
+          <Title title="[FE] 개발환경 구축하기" />
           <Label name="레이블 내용" backgroundColor="#000" color="#FFF" />
         </Box>
         <Details />
