60076f639ab5e17880a246ee8629cfb22bb6c703
최성익 <tjddlr92@gmail.com>
2020-06-15 18:49:51 +0900

    [FE/#8] Feat : 유저 목 데이터 생성

---
+++ FE/src/mock/data.js
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    id: "pororo1",
+    url:
+      "https://lh3.googleusercontent.com/lv3q0LxixvhfsBwhLKzpp53EPMQO92jq98uOKqT9sL0hv4gF8l9bl6SRN23FLxkeJH3ldxZt-PreP1zddg",
+  },
+  {
+    id: "pororo2",
+    url:
+      "https://lh3.googleusercontent.com/lv3q0LxixvhfsBwhLKzpp53EPMQO92jq98uOKqT9sL0hv4gF8l9bl6SRN23FLxkeJH3ldxZt-PreP1zddg",
+  },
+  {
+    id: "pororo3",
+    url:
+      "https://lh3.googleusercontent.com/lv3q0LxixvhfsBwhLKzpp53EPMQO92jq98uOKqT9sL0hv4gF8l9bl6SRN23FLxkeJH3ldxZt-PreP1zddg",
+  },
+];
