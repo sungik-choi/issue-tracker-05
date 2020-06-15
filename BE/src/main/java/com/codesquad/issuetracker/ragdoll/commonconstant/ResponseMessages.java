dae9f40f81d7f349c0cf2694331c9554bf17b314
ragdoll <museopkim0214@gmail.com>
2020-06-16 02:06:16 +0900

    [BE/#29] Feat : 수정 성공에 관한 응답 메시지 static 변수로 추가
    - ResponseMessage.SUCCESSFULLY_MODIFIED

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/commonconstant/ResponseMessages.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/commonconstant/ResponseMessages.java
@@ -2,4 +2,5 @@ package com.codesquad.issuetracker.ragdoll.commonconstant;
 
 public class ResponseMessages {
     public static String SUCCESSFULLY_CREATED = "등록이 완료 되었습니다.";
+    public static String SUCCESSFULLY_MODIFIED = "수정이 완료 되었습니다.";
 }
