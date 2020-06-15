2d4434c46a421817d2e91e655c7d961fedbcf1dd
ragdoll <museopkim0214@gmail.com>
2020-06-16 01:19:43 +0900

    [BE/#28] 이슈 상세 정보 반환 API 구현
    - 이슈 목록 API와 이슈 상세 정보 API 사이에서 Milestone의 progress 반환 여부에 차별성을 둘 지에 대해서는 고민이 필요

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/controller/IssueController_Ragdoll.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/controller/IssueController_Ragdoll.java
@@ -1,5 +1,6 @@
 package com.codesquad.issuetracker.ragdoll.controller;
 
+import com.codesquad.issuetracker.ragdoll.dto.DetailedInformationOfIssueDto;
 import com.codesquad.issuetracker.ragdoll.dto.ListOfIssuesDto;
 import com.codesquad.issuetracker.ragdoll.dto.SubmitNewIssueRequestDto;
 import com.codesquad.issuetracker.ragdoll.response.ApiResponse;
@@ -26,5 +27,10 @@ public class IssueController_Ragdoll {
     @PostMapping("/issues")
     public ResponseEntity<ApiResponse<String>> submit(@RequestBody SubmitNewIssueRequestDto submitNewIssueRequestDto) {
         return new ResponseEntity(ApiResponse.CREATED(issueService.submitNewIssue(submitNewIssueRequestDto)), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/issues/{issueId}")
+    public ResponseEntity<ApiResponse<DetailedInformationOfIssueDto>> showDetails(@PathVariable Long issueId) {
+        return new ResponseEntity(ApiResponse.OK(issueService.showIssueDetails(issueId)), HttpStatus.OK);
     }
 }
