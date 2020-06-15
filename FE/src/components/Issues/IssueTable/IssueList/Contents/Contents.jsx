221b3281e97c70620c9e75d22aa86478004128b5
최성익 <tjddlr92@gmail.com>
2020-06-15 18:05:46 +0900

    [FE/#8] Feat : Label 재사용 가능 컴포넌트로 변경
    - 컴포넌트 상위 디렉토리로 이동
    - name, backgroundColor, color를 받아 별도의 컴포넌트로 사용할 수 있도록 변경
    - 기본 매개변수는 defaultProps로 사용

--- FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
+++ FE/src/components/Issues/IssueTable/IssueList/Contents/Contents.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 
+import Label from "@Components/Labels";
 import Title from "./Title";
 import Details from "./Details";
-import Labels from "./Labels";
 
 const Contents = () => {
   return (
@@ -12,7 +12,7 @@ const Contents = () => {
       <Box display="flex" flexDirection="column" marginLeft={2}>
         <Box display="flex" alignItems="center">
           <Title />
-          <Labels />
+          <Label name="레이블 내용" backgroundColor="#000" color="#FFF" />
         </Box>
         <Details />
       </Box>
