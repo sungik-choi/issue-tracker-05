003e44d097c48156890f5d1565dbbcac6e96f13f
최성익 <tjddlr92@gmail.com>
2020-06-16 15:34:56 +0900

    [FE/#8] Refactor : selected -> selectedIssue 로 변경
    - 의미를 더 담기 위해 state명 변경

--- FE/src/components/Issues/IssueTable/IssueTable.jsx
+++ FE/src/components/Issues/IssueTable/IssueTable.jsx
@@ -12,23 +12,61 @@ import Issue from "./Issue/Issue";
 
 const IssueTable = () => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [selected, setSelected] = useState(new Set());
+  const [selectedIssue, setSelectedIssue] = useState(new Set());
 
-  const isSelected = (id) => selected.has(id);
+  const isSelectedIssue = (id) => selectedIssue.has(id);
+
   const handleCheckboxClick = (id) => {
-    const updatedSet = new Set(selected);
-    isSelected(id) ? updatedSet.delete(id) : updatedSet.add(id);
-    setSelected(updatedSet);
+    const updatedSet = new Set(selectedIssue);
+    isSelectedIssue(id) ? updatedSet.delete(id) : updatedSet.add(id);
+    setSelectedIssue(updatedSet);
   };
 
-  const handleMenuClick = (e) => setAnchorEl(e.currentTarget);
+  const handleAllCheckBoxClick = (event, issueList) => {
+    if (event.target.checked) {
+      const updatedSet = new Set(selectedIssue);
+      issueList.forEach(({ id }) => updatedSet.add(id));
+      setSelectedIssue(updatedSet);
+      return;
+    }
+    setSelectedIssue(new Set());
+  };
+
+  const isSelectedIssueAtLeastOne = (issueList) =>
+    selectedIssue.size > 0 && selectedIssue.size < issueList.length;
+
+  const isAllSelectedIssue = (issueList) =>
+    selectedIssue.size > 0 && selectedIssue.size === issueList.length;
+
+  const handleMenuClick = (event) => setAnchorEl(event.currentTarget);
+
   const handleMenuClose = () => setAnchorEl(null);
 
-  const headContents = (
+  const issueList = [
+    {
+      id: 1,
+      contents: (
+        <Issue id={1} isSelectedIssue={isSelectedIssue} clickHandler={handleCheckboxClick} />
+      ),
+    },
+    {
+      id: 2,
+      contents: (
+        <Issue id={2} isSelectedIssue={isSelectedIssue} clickHandler={handleCheckboxClick} />
+      ),
+    },
+  ];
+
+  const toolbar = (
     <>
       <Box display="flex" alignItems="center">
-        <Checkbox color="primary" />
-        <Typography>{selected.size} selected</Typography>
+        <Checkbox
+          color="primary"
+          indeterminate={isSelectedIssueAtLeastOne(issueList)}
+          checked={isAllSelectedIssue(issueList)}
+          onChange={(event) => handleAllCheckBoxClick(event, issueList)}
+        />
+        {!!selectedIssue.size && <Typography>{selectedIssue.size} selected</Typography>}
       </Box>
       <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleMenuClick}>
         Open Menu
@@ -47,20 +85,7 @@ const IssueTable = () => {
     </>
   );
 
-  const bodyContents = [
-    {
-      id: 1,
-      contents: <Issue id={1} isSelected={isSelected} clickHandler={handleCheckboxClick} />,
-    },
-    {
-      id: 2,
-      contents: <Issue id={2} isSelected={isSelected} clickHandler={handleCheckboxClick} />,
-    },
-  ];
-
-  return (
-    <CustomTable ariaLabel="issueTable" headContents={headContents} bodyContents={bodyContents} />
-  );
+  return <CustomTable ariaLabel="issueTable" headContents={toolbar} bodyContents={issueList} />;
 };
 
 export default IssueTable;
