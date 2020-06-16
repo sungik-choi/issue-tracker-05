87281a2a699ba3aa31ed4057d4eca2e040f8a158
Elllin <noto01@naver.com>
2020-06-16 16:00:24 +0900

    [FE/#10] Refactor : navigation에 nav태그 box로 변경

--- FE/src/components/Issues/Navigation/Navigation.jsx
+++ FE/src/components/Issues/Navigation/Navigation.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 import Filter from "./Filter/Filter";
 
-import styled from "styled-components";
+import Box from "@material-ui/core/Box";
 
 const Navigation = () => {
   return (
-    <Nav>
+    <Box component="nav" mt={6} mb={5}>
       <Filter />
-    </Nav>
+    </Box>
   );
 };
-
-const Nav = styled.nav`
-  margin: 50px 0 40px;
-`;
 
 export default Navigation;
