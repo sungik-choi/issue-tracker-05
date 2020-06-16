8601af0990bc1560be38a0aa1db16857fa41b53f
최성익 <tjddlr92@gmail.com>
2020-06-16 15:35:19 +0900

    [FE/#8] Feat : issue prop-types 설정

--- FE/src/components/Issues/IssueTable/Issue/Issue.jsx
+++ FE/src/components/Issues/IssueTable/Issue/Issue.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Box from "@material-ui/core/Box";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -8,17 +9,23 @@ import Contents from "./Contents/Contents";
 import StatusIcon from "./StatusIcon";
 import AvatarStack from "./AvatarStack";
 
-const Issue = ({ id, isSelected, clickHandler }) => {
+const Issue = ({ id, isSelectedIssue, clickHandler }) => {
   return (
     <>
       <Box display="flex" alignItems="center">
-        <Checkbox color="primary" checked={isSelected(id)} onClick={() => clickHandler(id)} />
+        <Checkbox color="primary" checked={isSelectedIssue(id)} onChange={() => clickHandler(id)} />
         <StatusIcon />
         <Contents />
       </Box>
       <AvatarStack userData={userList} />
     </>
   );
+};
+
+Issue.propTypes = {
+  id: PropTypes.number.isRequired,
+  isSelectedIssue: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Issue;
