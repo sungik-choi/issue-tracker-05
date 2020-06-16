30efc987a3b93e40ec6ebb6016f45cba9e165820
Elllin <noto01@naver.com>
2020-06-16 17:50:03 +0900

    [FE/#4] input 창 생성

--- FE/src/components/Issues/Navigation/Filter/Filter.jsx
+++ FE/src/components/Issues/Navigation/Filter/Filter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Menu from "./Menu";
+import SearchBar from "./SearchBar";
 
 const Filter = () => {
   return (
     <div>
-      <Menu></Menu>
+      <Menu />
+      <SearchBar />
     </div>
   );
 };
