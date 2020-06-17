f9218808b9d16f0ad01e6609d446a725f47c14f9
최성익 <tjddlr92@gmail.com>
2020-06-17 17:32:02 +0900

    [FE/#12] Feat : MenuList 옵션 추가
    - 필터에 사용하기 위해 clickHandler, avatar, name 옵션 추가
    - Typograhy API에 있는 속성을 활용해 스타일 변경
    - 주석 제거

--- FE/src/components/common/MenuList.jsx
+++ FE/src/components/common/MenuList.jsx
@@ -5,16 +5,37 @@ import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
-const MenuList = ({ text, title, clickHandler }) => {
+import CustomAvatar from "./CustomAvatar";
+
+const MenuList = ({ text, title, clickHandler, options: { avatar, name } }) => {
   const classes = useStyles();
 
   const boxClassName = title ? classes.titleBox : classes.popupBox;
   const boxText = title || text;
-  const boxFontWeight = title ? "bold" : "none";
+  const textVariant = title ? "subtitle2" : "body2";
 
   return (
-    <Box py={1} px={2} className={boxClassName} onClick={clickHandler}>
-      <Typography style={{ fontSize: "13px", fontWeight: boxFontWeight }}>{boxText}</Typography>
+    <Box
+      display="flex"
+      alignItems="center"
+      py={1}
+      px={2}
+      className={boxClassName}
+      onClick={clickHandler}
+    >
+      {avatar && (
+        <Box mr={1}>
+          <CustomAvatar id={avatar.id} url={avatar.url} />
+        </Box>
+      )}
+      <Typography variant={textVariant}>{boxText}</Typography>
+      {name && (
+        <Box ml={2}>
+          <Typography variant={textVariant} color="textSecondary">
+            {name}
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 };
@@ -37,12 +58,23 @@ MenuList.defaultProps = {
   text: "",
   title: "",
   clickHandler: null,
+  options: {
+    name: "",
+    avatar: null,
+  },
 };
 
 MenuList.propTypes = {
   text: PropTypes.string,
   title: PropTypes.string,
   clickHandler: PropTypes.func,
+  options: PropTypes.shape({
+    avatar: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+    name: PropTypes.string,
+  }),
 };
 
 export default MenuList;
