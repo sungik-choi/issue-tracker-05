34df7da9711fcea67ffa6c88ed73d73844150ee3
최성익 <tjddlr92@gmail.com>
2020-06-17 17:08:28 +0900

    [FE/#12] Feat : CustomAvatar 추가
    - 필터 메뉴의 사람 목록에서도 아바타를 쓰기위해 별도의 크기 조정된 커스텀 아바타 컴포넌트 추가

---
+++ FE/src/components/common/CustomAvatar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+
+const CustomAvatar = ({ id, url }) => {
+  const classes = useStyles();
+
+  return <Avatar key={id} alt={id} src={url} className={classes.avatar} />;
+};
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
+
+CustomAvatar.propTypes = {
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
+export default CustomAvatar;
