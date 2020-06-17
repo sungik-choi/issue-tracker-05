8e7b40ae3f1928adb62203cadebdef68eff410aa
최성익 <tjddlr92@gmail.com>
2020-06-17 17:38:45 +0900

    [FE/#12] Feat : MenuList 색상 수정
    - 역할에 맞는 색으로 수정
    - 주석 제거

--- FE/src/components/common/MenuList.jsx
+++ FE/src/components/common/MenuList.jsx
@@ -44,13 +44,13 @@ const useStyles = makeStyles((theme) => ({
   popupBox: {
     width: 250,
     cursor: "pointer",
-    borderTop: `1px solid${theme.palette.grey[300]}`,
+    borderTop: `1px solid ${theme.palette.divider}`,
     "&:hover": {
-      backgroundColor: theme.palette.grey[200],
+      backgroundColor: theme.palette.action.hover,
     },
   },
   titleBox: {
-    backgroundColor: theme.palette.grey[200],
+    backgroundColor: theme.palette.grey[100],
   },
 }));
 
@@ -78,11 +78,3 @@ MenuList.propTypes = {
 };
 
 export default MenuList;
-
-// 사용 용도는 두 가지가 있습니다
-// 1. title로써 상단에 사용
-// 2. menuList로써 hover와 마우스 클릭이 되는 list로 사용
-// title로 사용 시 : props로 title을 사용하고 title에 들어갈 text를 넣어주면 됩니다.
-// (예) <MenuList title="Filter lssues" />
-// menuList로 사용 시 : props로 text를 사용하고 list에 들어갈 text를 넣어주면 됩니다.
-// (예) <MenuList text="Open issues" />
