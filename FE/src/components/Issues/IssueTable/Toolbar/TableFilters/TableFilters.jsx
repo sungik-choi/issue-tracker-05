9cc255e8adc94c3f9cd288f3256f358055e31856
최성익 <tjddlr92@gmail.com>
2020-06-17 15:16:03 +0900

    [FE/#37] Refactor : 변수명 변경

--- FE/src/components/Issues/IssueTable/Toolbar/TableFilters/TableFilters.jsx
+++ FE/src/components/Issues/IssueTable/Toolbar/TableFilters/TableFilters.jsx
@@ -6,15 +6,15 @@ import Box from "@material-ui/core/Box";
 import FilterButton from "./FilterButton";
 
 const TableFilters = ({ bSelectedIssueExist }) => {
-  const markAsFilter = "Mark as";
+  const markAsFilterButton = "Mark as";
   const filterButtonList = ["Author", "Label", "Projects", "Milestones", "Assignees", "Sort"];
 
   return (
     <Box>
       {bSelectedIssueExist ? (
-        <FilterButton text={markAsFilter} />
+        <FilterButton text={markAsFilterButton} />
       ) : (
-        filterButtonList.map((buttonText) => <FilterButton text={buttonText} />)
+        filterButtonList.map((buttonText) => <FilterButton key={buttonText} text={buttonText} />)
       )}
     </Box>
   );
