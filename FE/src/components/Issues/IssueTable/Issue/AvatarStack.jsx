87c4c60926374e38a91f099c61dd34ed931a873d
최성익 <tjddlr92@gmail.com>
2020-06-17 17:08:58 +0900

    [FE/#12] Feat : AvatarStack 커스텀 아바타로 변경

--- FE/src/components/Issues/IssueTable/Issue/AvatarStack.jsx
+++ FE/src/components/Issues/IssueTable/Issue/AvatarStack.jsx
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import Avatar from "@material-ui/core/Avatar";
 import AvatarGroup from "@material-ui/lab/AvatarGroup";
+
+import CustomAvatar from "@Components/common/CustomAvatar";
 
 const AvatarStack = ({ userData }) => {
   const classes = useStyles();
@@ -14,7 +15,7 @@ const AvatarStack = ({ userData }) => {
     <Box display="flex" alignItems="center">
       <AvatarGroup max={MAX_AVATAR_NUM} classes={{ avatar: classes.avatar }}>
         {userData.map(({ id, url }) => (
-          <Avatar key={id} alt={id} src={url} />
+          <CustomAvatar key={id} id={id} url={url} />
         ))}
       </AvatarGroup>
     </Box>
@@ -23,8 +24,6 @@ const AvatarStack = ({ userData }) => {
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
-    width: theme.spacing(3),
-    height: theme.spacing(3),
     fontSize: "1rem",
     borderColor: `${theme.palette.common.white}`,
   },
