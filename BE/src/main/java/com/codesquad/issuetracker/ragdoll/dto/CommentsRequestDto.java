6c24b74d6195f5e19b02cd5868cf8d96a072a288
ragdoll <museopkim0214@gmail.com>
2020-06-18 21:24:59 +0900

    [BE/#48] Feat : 코멘트 등록 요청 DTO 추가
    - CommentsRequestDto

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dto/CommentsRequestDto.java
@@ -0,0 +1,24 @@
+package com.codesquad.issuetracker.ragdoll.dto;
+
+public class CommentsRequestDto {
+
+    private Long userId;
+
+    private String description;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
