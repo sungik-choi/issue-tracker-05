df03ff8d8d6c5fd60b1091ffb72839e94decf09c
ragdoll <museopkim0214@gmail.com>
2020-06-18 23:14:13 +0900

    [BE/#54] Feat : 마일스톤 리스트 반환 서비스 로직 구현
    - milestoneService.findAllMilestonesWithDetails()

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/service/MilestoneService_Ragdoll.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/service/MilestoneService_Ragdoll.java
@@ -3,6 +3,8 @@ package com.codesquad.issuetracker.ragdoll.service;
 import com.codesquad.issuetracker.ragdoll.dao.MilestoneDao_Ragdoll;
 import com.codesquad.issuetracker.ragdoll.domain.Issue;
 import com.codesquad.issuetracker.ragdoll.domain.Milestone;
+import com.codesquad.issuetracker.ragdoll.dto.ListOfMilestonesDto;
+import com.codesquad.issuetracker.ragdoll.vo.milestoneVO.MilestoneDetails;
 import com.codesquad.issuetracker.ragdoll.vo.milestoneVO.MilestoneInformation;
 import com.codesquad.issuetracker.ragdoll.vo.milestoneVO.MilestoneSummary;
 import org.springframework.stereotype.Service;
@@ -30,5 +32,25 @@ public class MilestoneService_Ragdoll {
                                                              .map(milestone -> MilestoneSummary.create(milestone.getId(), milestone.getTitle()))
                                                              .collect(Collectors.toSet());
         return MilestoneInformation.create(milestoneSummaries.size(), milestoneSummaries);
+    }
+
+    public ListOfMilestonesDto findAllMilestonesWithDetails() {
+        List<Milestone> milestones = milestoneDao.findAllMilestones();
+        List<MilestoneDetails> milestoneDetails = milestones.stream().map(milestone -> {
+            int countOfOpenedIssue = milestoneDao.findCountOfOpenedIssueById(milestone.getId(), true);
+            int countOfClosedIssue = milestoneDao.findCountOfOpenedIssueById(milestone.getId(), false);
+            int countOfAllIssue = (countOfOpenedIssue + countOfClosedIssue == 0) ? 1 : countOfOpenedIssue + countOfClosedIssue;
+            double progress = ((double) countOfClosedIssue / countOfAllIssue) * 100;
+            return new MilestoneDetails.Builder()
+                                       .milestoneId(milestone.getId())
+                                       .milestoneTitle(milestone.getTitle())
+                                       .description(milestone.getDescription())
+                                       .dueDate(milestone.getDueDate())
+                                       .progress(progress)
+                                       .countOfOpenedIssue(countOfOpenedIssue)
+                                       .countOfClosedIssue(countOfClosedIssue)
+                                       .build();
+        }).collect(Collectors.toList());
+        return new ListOfMilestonesDto.Builder().milestones(milestoneDetails).build();
     }
 }
