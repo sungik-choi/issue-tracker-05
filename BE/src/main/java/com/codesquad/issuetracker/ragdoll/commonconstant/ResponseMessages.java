e30081c70a3ab1beceefcab2013fc33d4f881220
ragdoll <museopkim0214@gmail.com>
2020-06-18 23:05:31 +0900

    [BE#53] Feat : 삭제 응답 메시지 static 문자열 추가
    - ResponseMessages.SUCCESSFULLY_DELETED

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/commonconstant/ResponseMessages.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/commonconstant/ResponseMessages.java
@@ -3,4 +3,5 @@ package com.codesquad.issuetracker.ragdoll.commonconstant;
 public class ResponseMessages {
     public static String SUCCESSFULLY_CREATED = "등록이 완료 되었습니다.";
     public static String SUCCESSFULLY_MODIFIED = "수정이 완료 되었습니다.";
+    public static String SUCCESSFULLY_DELETED = "삭제가 완료 되었습니다.";
 }
