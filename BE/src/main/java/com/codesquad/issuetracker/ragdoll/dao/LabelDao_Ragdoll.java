3ce8e743c16d1c0fc7816589fb85f5e782b52293
ragdoll <museopkim0214@gmail.com>
2020-06-18 23:03:27 +0900

    [BE/#52] Feat : 레이블 업데이트 관련 Dao 쿼리 추가
    - namedParameterJdbcTemplate 활용

--- BE/src/main/java/com/codesquad/issuetracker/ragdoll/dao/LabelDao_Ragdoll.java
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dao/LabelDao_Ragdoll.java
@@ -3,6 +3,9 @@ package com.codesquad.issuetracker.ragdoll.dao;
 import com.codesquad.issuetracker.ragdoll.domain.Label;
 import com.codesquad.issuetracker.ragdoll.vo.labelVO.LabelSummary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -13,8 +16,12 @@ public class LabelDao_Ragdoll {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     public LabelDao_Ragdoll(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     public void createNewLabel(String labelName, String description, String hexCode) {
         String sql = "INSERT INTO label (name, description, hex_code) VALUES (?, ?, ?)";
@@ -34,5 +41,15 @@ public class LabelDao_Ragdoll {
         String sql = "SELECT id, name, description, hex_code FROM label";
         return jdbcTemplate.query(sql, (rs, rowNum) -> Label.create(rs.getInt("id"), rs.getString("name"),
                                                                     rs.getString("description"), rs.getString("hex_code")));
+    }
+
+    public void updateLabel(Integer labelId, String labelName, String description, String hexCode) {
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                                                .addValue("labelId", labelId)
+                                                .addValue("labelName", labelName)
+                                                .addValue("description", description)
+                                                .addValue("hexCode", hexCode);
+        String sql = "UPDATE label SET name = :labelName, description = :description, hex_code = :hexCode WHERE id = :labelId";
+        namedParameterJdbcTemplate.update(sql, namedParameters);
     }
 }
