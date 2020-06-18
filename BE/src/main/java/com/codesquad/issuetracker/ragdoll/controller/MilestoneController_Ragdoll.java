98cedda64ff4968d991691875a74f25290ccf9a7
ragdoll <museopkim0214@gmail.com>
2020-06-18 23:09:56 +0900

    [BE/#54] Feat : 마일스톤 목록 요청 컨트롤러 로직 추가
    -> milestoneService.findAllMilestonesWithDetails()

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/controller/MilestoneController_Ragdoll.java
@@ -0,0 +1,26 @@
+package com.codesquad.issuetracker.ragdoll.controller;
+
+import com.codesquad.issuetracker.ragdoll.dto.ListOfMilestonesDto;
+import com.codesquad.issuetracker.ragdoll.response.ApiResponse;
+import com.codesquad.issuetracker.ragdoll.service.MilestoneService_Ragdoll;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ragdoll")
+public class MilestoneController_Ragdoll {
+
+    private MilestoneService_Ragdoll milestoneService;
+
+    public MilestoneController_Ragdoll(MilestoneService_Ragdoll milestoneService) {
+        this.milestoneService = milestoneService;
+    }
+
+    @GetMapping("/milestones")
+    public ResponseEntity<ApiResponse<ListOfMilestonesDto>> listing() {
+        return new ResponseEntity(ApiResponse.OK(milestoneService.findAllMilestonesWithDetails()), HttpStatus.OK);
+    }
+}
