cf88db90feb132c17eec3e0c6dd048b351ce4981
최성익 <tjddlr92@gmail.com>
2020-06-18 15:23:24 +0900

    [FE/#45] Feat : Avatar 툴팁 추가
    - 아바타에 마우스를 hover하면 해당 유저의 id 표시

--- FE/src/components/common/CustomAvatar.jsx
+++ FE/src/components/common/CustomAvatar.jsx
@@ -3,11 +3,16 @@ import PropTypes from "prop-types";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const CustomAvatar = ({ id, url }) => {
   const classes = useStyles();
 
-  return <Avatar alt={id} src={url} className={classes.avatar} />;
+  return (
+    <Tooltip title={id}>
+      <Avatar alt={id} src={url} className={classes.avatar} />
+    </Tooltip>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
