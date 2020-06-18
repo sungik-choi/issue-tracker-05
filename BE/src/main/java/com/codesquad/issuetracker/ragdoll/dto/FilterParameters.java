c7b938932aaf9a1f12ba9f3c7ba96c44d5de4948
ragdoll <museopkim0214@gmail.com>
2020-06-19 02:29:45 +0900

    [BE/#58] Feat : 필터링 파라미터 클래스 추가
    - default 값 없이 클래스로 처리
    - FilterParameters {}
    - open
    - author
    - label
    - milestones
    - assignee

---
+++ BE/src/main/java/com/codesquad/issuetracker/ragdoll/dto/FilterParameters.java
@@ -0,0 +1,54 @@
+package com.codesquad.issuetracker.ragdoll.dto;
+
+public class FilterParameters {
+
+    private Boolean open;
+
+    private Long author;
+
+    private Integer label;
+
+    private Integer milestones;
+
+    private Long assignee;
+
+    public Boolean getOpen() {
+        return open;
+    }
+
+    public void setOpen(Boolean open) {
+        this.open = open;
+    }
+
+    public Long getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Long author) {
+        this.author = author;
+    }
+
+    public Integer getLabel() {
+        return label;
+    }
+
+    public void setLabel(Integer label) {
+        this.label = label;
+    }
+
+    public Integer getMilestones() {
+        return milestones;
+    }
+
+    public void setMilestones(Integer milestones) {
+        this.milestones = milestones;
+    }
+
+    public Long getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Long assignee) {
+        this.assignee = assignee;
+    }
+}
