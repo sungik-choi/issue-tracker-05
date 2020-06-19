480425e571f2a168b9f3e2707c7dd3aafa0ab7af
Hamill210 <han95210@naver.com>
2020-06-19 13:57:43 +0900

    [BE] Modify : ApiResponse1 클래스 ApiResponse로 변경

---
+++ BE/src/main/java/com/codesquad/issuetracker/hamill/response/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.codesquad.issuetracker.hamill.response;
+
+public class ApiResponse<T> {
+
+    private boolean status;
+
+    private T response;
+
+    public ApiResponse() {}
+
+    private ApiResponse(boolean status, T response) {
+        this.status = status;
+        this.response = response;
+    }
+
+    public static <T> ApiResponse<T> OK(T response) {
+        return new ApiResponse<>(true, response);
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+}
