a208cf1fa4c1e77371bf6c8ee34968d8b5242e71
최성익 <tjddlr92@gmail.com>
2020-06-19 13:35:37 +0900

    [FE/#65] Feat : 시간 차이 계산 유틸 함수 작성

---
+++ FE/src/utils/calcTimeDiff.js
@@ -0,0 +1,27 @@
+import moment from "moment";
+
+const DAY = "day";
+const HOUR = "hour";
+const MINUTE = "minute";
+const SECOND = "second";
+
+const getPluralWord = (target, word) => (target > 1 ? `${word}s` : word);
+
+const calcTimeDiff = (time) => {
+  const previousTime = moment(time, "YYYY-MM-DD HH:mm:ss");
+  const currentTime = moment();
+
+  const day = moment.duration(currentTime.diff(previousTime)).days();
+  if (day) return `${day} ${getPluralWord(day, DAY)}`;
+
+  const hour = moment.duration(currentTime.diff(previousTime)).hours();
+  if (hour) return `${hour} ${getPluralWord(hour, HOUR)}`;
+
+  const minute = moment.duration(currentTime.diff(previousTime)).minutes();
+  if (minute) return `${minute} ${getPluralWord(minute, MINUTE)}`;
+
+  const second = moment.duration(currentTime.diff(previousTime)).seconds();
+  if (second) return `${second} ${getPluralWord(second, SECOND)}`;
+};
+
+export default calcTimeDiff;
