fce171a389d80508d5599419d492ced4d5ad4433
최성익 <tjddlr92@gmail.com>
2020-06-19 21:43:21 +0900

    [FE/#69] Refactor : 라벨/마일스톤 카운트 navigation으로 이동
    - LinkButtons는 재사용 컴포넌트므로 내부에서 상태값을 부르면 안됨.
    - count가 없이 사용되는 경우도 있으므로 Navigation으로 값을 주입

--- FE/src/components/Issues/Navigation/Navigation.jsx
+++ FE/src/components/Issues/Navigation/Navigation.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Box from "@material-ui/core/Box";
 
 import PrimaryButton from "@Components/common/PrimaryButton";
 import LinkButtons from "@Components/common/LinkButtons/LinkButtons";
+import { IssueListContext } from "@Contexts/issueListContext";
 import Filter from "./Filter/Filter";
 
 const Navigation = () => {
   const NEW_ISSUE_BTN_TEXT = "New issue";
 
+  const {
+    issueList: {
+      labelInfo: { countOfLabel },
+      milestoneInfo: { countOfMilestone },
+    },
+  } = useContext(IssueListContext);
+
   return (
     <Box component="nav" mt={6} mb={2} maxHeight={38} display="flex" justifyContent="space-between">
       <Filter />
-      <LinkButtons />
+      <LinkButtons labelCount={countOfLabel} milestoneCount={countOfMilestone} />
       <PrimaryButton text={NEW_ISSUE_BTN_TEXT} clickHandler={null} />
     </Box>
   );
--- FE/src/components/common/LinkButtons/LinkButtons.jsx
+++ FE/src/components/common/LinkButtons/LinkButtons.jsx
@@ -1,30 +1,32 @@
-import React, { useContext } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import LocalOfferOutlinedIcon from "@material-ui/icons/LocalOfferOutlined";
 import OutlinedFlagIcon from "@material-ui/icons/OutlinedFlag";
 
-import { IssueListContext } from "@Contexts/issueListContext";
-
 import LinkButton from "./LinkButton";
 
-const LinkButtons = () => {
+const LinkButtons = ({ labelCount, milestoneCount }) => {
   const LABELS = "Labels";
   const MILESTONES = "Milestones";
 
-  const {
-    issueList: {
-      labelInfo: { countOfLabel },
-      milestoneInfo: { countOfMilestone },
-    },
-  } = useContext(IssueListContext);
-
   return (
     <ButtonGroup variant="outlined">
-      <LinkButton text={LABELS} icon={<LocalOfferOutlinedIcon />} count={countOfLabel} />
-      <LinkButton text={MILESTONES} icon={<OutlinedFlagIcon />} count={countOfMilestone} />
+      <LinkButton text={LABELS} icon={<LocalOfferOutlinedIcon />} count={labelCount} />
+      <LinkButton text={MILESTONES} icon={<OutlinedFlagIcon />} count={milestoneCount} />
     </ButtonGroup>
   );
+};
+
+LinkButtons.defaultProps = {
+  labelCount: null,
+  milestoneCount: null,
+};
+
+LinkButtons.propTypes = {
+  labelCount: PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(null)]),
+  milestoneCount: PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(null)]),
 };
 
 export default LinkButtons;
