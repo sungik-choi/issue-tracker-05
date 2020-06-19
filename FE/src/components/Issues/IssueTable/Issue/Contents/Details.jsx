6d219b4a25553731850ea81e695ba045ec375a31
최성익 <tjddlr92@gmail.com>
2020-06-19 13:36:22 +0900

    [FE/#65] Feat : 시간 계산 함수 적용

--- FE/src/components/Issues/IssueTable/Issue/Contents/Details.jsx
+++ FE/src/components/Issues/IssueTable/Issue/Contents/Details.jsx
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
-// ! 시간 계산 로직 필요
+import calcTimeDiff from "@Utils/calcTimeDiff";
 
 const Details = ({ id, time, author }) => {
-  const text = `#${id} opened ${time} by ${author}`;
+  const text = `#${id} opened ${calcTimeDiff(time)} ago by ${author}`;
 
   return (
     <Box color="text.secondary">
