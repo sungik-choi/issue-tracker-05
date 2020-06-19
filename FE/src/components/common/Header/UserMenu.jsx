bbe517ba1ab808d53ec8a5ba703558093e7f55d3
최성익 <tjddlr92@gmail.com>
2020-06-19 21:13:18 +0900

    [FE/#69] Feat : UserMenu 컴포넌트에 icon 추가

--- FE/src/components/common/Header/UserMenu.jsx
+++ FE/src/components/common/Header/UserMenu.jsx
@@ -5,9 +5,12 @@ import Popover from "@material-ui/core/Popover";
 import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
 
 import CustomAvatar from "@Components/common/CustomAvatar";
 
@@ -34,10 +37,16 @@ const UserMenu = ({ id, url }) => {
           >
             <List>
               <ListItem>
+                <ListItemIcon>
+                  <AccountCircleOutlinedIcon />
+                </ListItemIcon>
                 <ListItemText primary={id} />
               </ListItem>
               <Divider />
               <ListItem button>
+                <ListItemIcon>
+                  <ExitToAppOutlinedIcon />
+                </ListItemIcon>
                 <ListItemText primary={SIGN_OUT} />
               </ListItem>
             </List>
