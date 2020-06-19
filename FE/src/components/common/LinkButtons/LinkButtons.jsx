380fb028f109cd2ad7ff12d6c89f7d86c6043e82
최성익 <tjddlr92@gmail.com>
2020-06-19 21:37:06 +0900

    [FE/#69] Feat : LinkButton count 상태값 연동

--- FE/src/components/common/LinkButtons/LinkButtons.jsx
+++ FE/src/components/common/LinkButtons/LinkButtons.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import LocalOfferOutlinedIcon from "@material-ui/icons/LocalOfferOutlined";
 import OutlinedFlagIcon from "@material-ui/icons/OutlinedFlag";
 
+import { IssueListContext } from "@Contexts/issueListContext";
 import LinkButton from "./LinkButton";
-
-const labelCount = 8;
-const milestonesCount = 3; // 상태값으로 변경하기
 
 const LinkButtons = () => {
   const LABELS = "Labels";
   const MILESTONES = "Milestones";
 
+  const {
+    issueList: {
+      labelInfo: { countOfLabel },
+      milestoneInfo: { countOfMilestone },
+    },
+  } = useContext(IssueListContext);
+
   return (
     <ButtonGroup variant="outlined">
-      <LinkButton text={LABELS} icon={<LocalOfferOutlinedIcon />} count={labelCount} />
-      <LinkButton text={MILESTONES} icon={<OutlinedFlagIcon />} count={milestonesCount} />
+      <LinkButton text={LABELS} icon={<LocalOfferOutlinedIcon />} count={countOfLabel} />
+      <LinkButton text={MILESTONES} icon={<OutlinedFlagIcon />} count={countOfMilestone} />
     </ButtonGroup>
   );
 };
