f1768ca15538971a92c5caa3dbde441a019f6934
최성익 <tjddlr92@gmail.com>
2020-06-19 21:06:43 +0900

    [FE/#69] Feat : userContext 기본값 mockData 설정
    - userReducer fix

--- FE/src/contexts/userContext.js
+++ FE/src/contexts/userContext.js
@@ -3,7 +3,13 @@ import PropTypes from "prop-types";
 
 import { userReducer } from "@Reducers/userReducer";
 
-export const initialState = {};
+// ! Mock User Data
+
+export const initialState = {
+  id: "sungik-choi",
+  avatarUrl:
+    "https://lh3.googleusercontent.com/lv3q0LxixvhfsBwhLKzpp53EPMQO92jq98uOKqT9sL0hv4gF8l9bl6SRN23FLxkeJH3ldxZt-PreP1zddg",
+};
 
 export const UserContext = createContext();
 
