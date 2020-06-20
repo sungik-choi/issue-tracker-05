271bae360fa876cd502d27a7a61da2d526b0625b
Elllin <noto01@naver.com>
2020-06-21 02:25:56 +0900

    [FE/#70] Refactor : save,close버튼 layout수정
    input과flex되도록 변경

--- FE/src/components/DetailedIssue/Header/Title/InputBox.jsx
+++ FE/src/components/DetailedIssue/Header/Title/InputBox.jsx
@@ -5,6 +5,7 @@ import { data } from "@Mock/detailedIssue";
 
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import { makeStyles } from "@material-ui/core/styles";
 
 const InputBox = ({ title, onChange, onClickSave, onClickClose }) => {
@@ -32,14 +33,16 @@ const InputBox = ({ title, onChange, onClickSave, onClickClose }) => {
 
   return (
     <form className={classes.input}>
-      {textField}
-      <Box display="flex">
-        <Button variant="contained" onClick={onClickSave}>
-          {SAVE_BTN}
-        </Button>
-        <Button variant="contained" onClick={onClickClose}>
-          {CLOSE_BTN}
-        </Button>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        {textField}
+        <ButtonGroup>
+          <Button variant="contained" onClick={onClickSave} className={classes.button}>
+            {SAVE_BTN}
+          </Button>
+          <Button variant="contained" onClick={onClickClose}>
+            {CLOSE_BTN}
+          </Button>
+        </ButtonGroup>
       </Box>
     </form>
   );
@@ -47,7 +50,11 @@ const InputBox = ({ title, onChange, onClickSave, onClickClose }) => {
 
 const useStyles = makeStyles((theme) => ({
   input: {
-    width: "80%",
+    width: "100%",
+  },
+  button: {
+    marginLeft: theme.spacing(6),
+    marginRight: theme.spacing(2),
   },
 }));
 
