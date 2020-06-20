e631ec4dcd6e1d1ee11b8c26d3da4659a571d440
Elllin <noto01@naver.com>
2020-06-20 23:00:29 +0900

    [FE/#70] Feat : 타이틀, edit 버튼 layout
    양쪽 끝으로 layout

--- FE/src/components/DetailedIssue/Header/Title/Title.jsx
+++ FE/src/components/DetailedIssue/Header/Title/Title.jsx
@@ -4,6 +4,7 @@ import { data } from "@Mock/detailedIssue";
 import EditButton from "./EditButton";
 
 import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
 const Title = () => {
@@ -13,13 +14,13 @@ const Title = () => {
   const id = data.issue.issueId;
 
   return (
-    <>
+    <Box display="flex" alignItems="center" justifyContent="space-between">
       <Typography variant="h3" component="h2" gutterBottom>
         <span>{title}</span>
         <span className={classes.issueNum}>#{id}</span>
       </Typography>
       <EditButton />
-    </>
+    </Box>
   );
 };
 
