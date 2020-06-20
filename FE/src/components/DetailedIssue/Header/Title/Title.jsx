ac3c5cb5be97539f3bc187f3b80474506ea19513
Elllin <noto01@naver.com>
2020-06-20 23:44:08 +0900

    [FE/#70] Feat : edit버튼 토글기능
    버튼 클릭시 input창과 타이틀 번갈아가며 렌더링

--- FE/src/components/DetailedIssue/Header/Title/Title.jsx
+++ FE/src/components/DetailedIssue/Header/Title/Title.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { data } from "@Mock/detailedIssue";
-import EditButton from "./EditButton";
 import InputBox from "./InputBox";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const Title = () => {
   const classes = useStyles();
 
+  const EIDT_BTN_TEXT = "Edit";
+
   const title = data.issue.issueTitle;
   const id = data.issue.issueId;
 
+  const [isEdit, setIsEdit] = useState(false);
+
+  const onClickEdit = () => {
+    setIsEdit((prevState) => !prevState);
+  };
+
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
-      {/* <Typography variant="h3" component="h2" gutterBottom>
-        <span>{title}</span>
-        <span className={classes.issueNum}>#{id}</span>
-      </Typography> */}
-      <InputBox />
-
-      <EditButton />
+      {isEdit ? (
+        <InputBox />
+      ) : (
+        <Typography variant="h3" component="h2" gutterBottom>
+          <span>{title}</span>
+          <span className={classes.issueNum}>#{id}</span>
+        </Typography>
+      )}
+      <Button variant="contained" onClick={onClickEdit}>
+        {EIDT_BTN_TEXT}
+      </Button>
     </Box>
   );
 };
