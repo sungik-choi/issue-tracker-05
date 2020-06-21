804e58c2155acfbbe4e2a93237aa780e9b6c9d31
Hamill210 <han95210@naver.com>
2020-06-21 23:47:44 +0900

    [BE/#18] Feat : Request body에 있는 JSON 응답을 DTO 객체로 받는다

--- BE/src/main/java/com/codesquad/issuetracker/hamill/dto/RequestNewIssueDto.java
+++ BE/src/main/java/com/codesquad/issuetracker/hamill/dto/RequestNewIssueDto.java
@@ -1,17 +1,61 @@
 package com.codesquad.issuetracker.hamill.dto;
 
-import com.codesquad.issuetracker.hamill.dto.label.LabelDto;
-import com.codesquad.issuetracker.hamill.dto.milestone.MilestoneDto;
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
 public class RequestNewIssueDto {
+
     private Long userId;
+
     private String title;
+
     private String description;
-    private List<AssigneeDto> allocatedAssignees;
-    private List<LabelDto> attachedLabels;
-    private MilestoneDto milestone;
+
+    private RequestNewIssueDto(Long userId, String title, String description) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public static RequestNewIssueDto of(Long userId, String title, String description) {
+        return new Builder()
+                .userId(userId)
+                .title(title)
+                .description(description)
+                .build();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private static class Builder {
+        private Long userId;
+        private String title;
+        private String description;
+
+        private Builder userId(Long userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        private Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        private Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        private RequestNewIssueDto build() {
+            return new RequestNewIssueDto(userId, title, description);
+        }
+    }
 }
--- BE/src/main/java/com/codesquad/issuetracker/main/dto/RequestNewIssueDto.java
+++ BE/src/main/java/com/codesquad/issuetracker/main/dto/RequestNewIssueDto.java
@@ -1,8 +1,5 @@
 package com.codesquad.issuetracker.main.dto;
 
-import com.codesquad.issuetracker.hamill.dto.AssigneeDto;
-import com.codesquad.issuetracker.hamill.dto.label.LabelDto;
-import com.codesquad.issuetracker.hamill.dto.milestone.MilestoneDto;
 import lombok.Getter;
 
 import java.util.List;
@@ -12,7 +9,4 @@ public class RequestNewIssueDto {
     private Long userId;
     private String title;
     private String description;
-    private List<AssigneeDto> allocatedAssignees;
-    private List<LabelDto> attachedLabels;
-    private MilestoneDto milestone;
 }
