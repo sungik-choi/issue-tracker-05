51b0f31dca223317f0741abc4b1647413fe1d480
최성익 <tjddlr92@gmail.com>
2020-06-23 19:49:24 +0900

    [FE/#60] Fix : 오타 수정

--- FE/src/components/common/LinkButtons/LinkButtons.jsx
+++ FE/src/components/common/LinkButtons/LinkButtons.jsx
@@ -24,7 +24,7 @@ const LinkButtons = ({ labelCount, milestoneCount, activeButton }) => {
         disableElevation
       />
       <LinkButton
-        primary={isMilestonesButtonActivated && "primary"}
+        color={isMilestonesButtonActivated && "primary"}
         text={MILESTONES}
         icon={<OutlinedFlagIcon />}
         count={milestoneCount}
