cb5bc4e4585cc87d727eb8c907eb3cb142002cf9
최성익 <tjddlr92@gmail.com>
2020-06-24 23:17:02 +0900

    [FE/#60] Feat : Label Table 데이터 렌더링

--- FE/src/components/Labels/LabelTable.jsx
+++ FE/src/components/Labels/LabelTable.jsx
@@ -1,28 +1,40 @@
 import React, { useContext } from "react";
 
 import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
+import Label from "@Components/common/Label";
 import CustomTable from "@Components/common/CustomTable";
+import CustomButton from "@Components/common/CustomButton";
+
 import { LabelListContext } from "@Contexts/labelListContext";
 
 const LabelTable = () => {
   const {
     labelList: { countOfLabel, labels },
-    labelListDispatch,
   } = useContext(LabelListContext);
 
-  const headContents = <Box fontWeight="800">8 Labels</Box>;
-  const bodyContents = [
-    {
-      id: 1,
-      contents: "1",
-    },
-    {
-      id: 2,
-      contents: "2",
-    },
-  ];
+  const headContents = <Box fontWeight="800">{countOfLabel} Labels</Box>;
+  const bodyContents = labels.map(({ id, name, description, backgroundColor, color }) => ({
+    id,
+    contents: (
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={4}>
+          <Label name={name} size="medium" backgroundColor={backgroundColor} color={color} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography>{description}</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Box display="flex" justifyContent="flex-end" alignItems="center">
+            <CustomButton variant="text" text="Edit" color="default" />
+            <CustomButton variant="text" text="Delete" color="default" />
+          </Box>
+        </Grid>
+      </Grid>
+    ),
+  }));
 
   return (
     <CustomTable ariaLabel="label-table" headContents={headContents} bodyContents={bodyContents} />
