5ca62bda787e86c08496279ed4225a220953ec0d
최성익 <tjddlr92@gmail.com>
2020-06-25 01:55:57 +0900

    [FE/#15] Feat : 토큰 저장, 토큰 삭제 기능 구현
    - pipe 함수를 사용해 토큰 저장기능 리팩토링도 함께 진행

--- FE/src/pages/Login.jsx
+++ FE/src/pages/Login.jsx
@@ -3,25 +3,28 @@ import React, { useContext, useEffect } from "react";
 import jwtDecode from "jwt-decode";
 import { useCookies } from "react-cookie";
 
+import pipe from "@Utils/pipe";
+
 import { UserContext } from "@Contexts/userContext";
 import { setUserData } from "@Reducers/userReducer";
 
 import LoginForm from "@Components/Login/LoginForm";
 
-const TOKEN = "token";
+import { TOKEN } from "@Constants/constants";
 
 const Login = () => {
   document.cookie =
     "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdmF0YXJVcmwiOiJodHRwczovL2F2YXRhcnMyLmdpdGh1YnVzZXJjb250ZW50LmNvbS91LzU4MjA5MDA5P3Y9NCIsIm5hbWUiOiJTdW5naWsgQ2hvaSIsImlkIjo1ODIwOTAwOSwiZXhwIjoxNTkzMDQ1MDI5fQ.u8oD16_vVEfNumcueeZy2Re-FUMIVb-DLNTcdeDShz8";
-  const [cookies, setCookies] = useCookies([TOKEN]);
+  const [cookies] = useCookies([TOKEN]);
   const { token } = cookies;
 
   const { userDispatch } = useContext(UserContext);
 
-  useEffect(() => {
-    const decodedToken = jwtDecode(token);
-    userDispatch(setUserData(decodedToken));
-  }, []);
+  useEffect(
+    () => pipe(jwtDecode, setUserData, userDispatch)(token),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
   return <LoginForm />;
 };
