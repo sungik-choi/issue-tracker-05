b74864c55aac2fe06651d8c94012fe566b8afeac
Elllin <noto01@naver.com>
2020-06-25 16:01:09 +0900

    [FE/#74] Refactor : 불필요한 파일 삭제

--- FE/src/hooks/useFetch.js
+++ FE/src/hooks/useFetch.js
@@ -1,63 +1,26 @@
-// import { useState, useEffect } from "react";
-
-// const useFetch = ({ url, actionType: { successAction, errorAction }, dispatch }) => {
-//   const [loading, setLoading] = useState(true);
-
-//   const getData = async () => {
-//     const data = await fetch(url);
-//     const { response } = await data.json();
-
-//     try {
-//       console.log("[log] fetch : ", response);
-//       dispatch(successAction(response));
-//     } catch (e) {
-//       dispatch(errorAction());
-//     }
-//     setLoading(false);
-//   };
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   return loading;
-// };
-
-// export default useFetch;
-
 import { useState, useEffect } from "react";
 
-const useFetch = ({
-  url,
-  deps = [],
-  dispatch,
-  actionType: { fetchSuccess, fetchError },
-  option = {},
-  skip = false,
-}) => {
-  console.log(url);
+const useFetch = ({ url, actionType: { successAction, errorAction }, dispatch }) => {
   const [loading, setLoading] = useState(true);
+
   const getData = async () => {
-    const data = await fetch(url, option);
+    const data = await fetch(url);
     const { response } = await data.json();
 
     try {
-      console.log("[log] data : ", response);
-      dispatch(fetchSuccess(response));
+      console.log("[log] fetch : ", response);
+      dispatch(successAction(response));
     } catch (e) {
-      console.log("[log] error : ", response);
-      dispatch(fetchError());
+      dispatch(errorAction());
     }
     setLoading(false);
   };
 
   useEffect(() => {
-    if (skip) return;
     getData();
-    // eslint-disable-next-line
-  }, deps);
+  }, []);
 
-  return { loading, getData };
+  return loading;
 };
 
 export default useFetch;
--- FE/src/utils/pipe.js
+++
@@ -1,3 +0,0 @@
-const pipe = (...functions) => (arg) => functions.reduce((prev, fn) => fn(prev), arg);
-
-export default pipe;
