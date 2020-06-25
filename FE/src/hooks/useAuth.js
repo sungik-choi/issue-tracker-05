dc710e1945386472c75867445bd92d8928cd41b1
Sungik Choi <tjddlr92@gmail.com>
2020-06-25 15:56:42 +0900

    [FE/#87] Fix : 하드코딩된 토큰 제거

--- FE/src/hooks/useAuth.js
+++ FE/src/hooks/useAuth.js
@@ -9,9 +9,6 @@ import { setUserData } from "@Reducers/userReducer";
 import { TOKEN } from "@Constants/constants";
 
 const useAuth = () => {
-  document.cookie =
-    "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdmF0YXJVcmwiOiJodHRwczovL2F2YXRhcnMyLmdpdGh1YnVzZXJjb250ZW50LmNvbS91LzU4MjA5MDA5P3Y9NCIsIm5hbWUiOiJTdW5naWsgQ2hvaSIsImlkIjo1ODIwOTAwOSwiZXhwIjoxNTkzMDQ1MDI5fQ.u8oD16_vVEfNumcueeZy2Re-FUMIVb-DLNTcdeDShz8";
-
   const [{ token }] = useCookies([TOKEN]);
   const { userDispatch } = useContext(UserContext);
   const [isAuthenticated, setIsAuthenticated] = useState(!!token);
