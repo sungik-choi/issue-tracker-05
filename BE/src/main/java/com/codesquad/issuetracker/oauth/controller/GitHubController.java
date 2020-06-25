1a5ccee06b00bfecac07862806e5bd894f13f49a
Hamill210 <han95210@naver.com>
2020-06-25 16:38:47 +0900

    no message

--- BE/src/main/java/com/codesquad/issuetracker/oauth/controller/GitHubController.java
+++ BE/src/main/java/com/codesquad/issuetracker/oauth/controller/GitHubController.java
@@ -4,6 +4,7 @@ import com.codesquad.issuetracker.oauth.service.GitHubOauthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import javax.websocket.server.PathParam;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
 public class GitHubController {
     private static final Logger logger = LoggerFactory.getLogger(GitHubController.class);
 
